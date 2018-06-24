84e46ef7189919f4411052053e8f01bd411963ab
xuchaoxi <xuchaoxikb@163.com>
2018-06-24 16:54:33 +0800

    first commit

---
+++ utils/util.py
@@ -0,0 +1,75 @@
+from __future__ import print_function, division
+
+import os
+import torch
+import shutil
+from collections import OrderedDict
+
+def save_checkpoint(state, is_best, filename='checkpoint.pth.tar', prefix=''):
+    if prefix != '' and not os.path.exists(prefix):
+        os.makedirs(prefix)
+    newfile = os.path.join(prefix, filename)
+    torch.save(state, newfile)
+    print ('model saved at {}'.format(newfile))
+    if is_best:
+        shutil.copyfile(newfile, os.path.join(checkpoint,'model_best.pth.tar'))
+
+def load_checkpoint(filename='model_best.pth.tar', modeldir=''):
+    checkpoint = torch.load(os.path.join(modeldir, filename))
+    return checkpoint
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=0):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / (1e-6 + self.count)
+
+    def __str__(self):
+    """String representation for logging
+    """
+    # for values that should be recorded exactly e.g. iteration number
+    if self.count == 0:
+        return str(self.val)
+    # for stats
+    return '%.4f (%.4f)' % (self.val, self.avg)
+
+class LogCollector(object):
+    """A collection of logging objects that can change from train to val"""
+
+    def __init__(self):
+        # to keep the order of logged variables deterministic
+        self.meters = OrderedDict()
+
+    def update(self, k, v, n=0):
+        # create a new meter if previously not recorded
+        if k not in self.meters:
+            self.meters[k] = AverageMeter()
+        self.meters[k].update(v, n)
+
+    def __str__(self):
+        """Concatenate the meters in one log line
+        """
+        s = ''
+        for i, (k, v) in enumerate(self.meters.iteritems()):
+            if i > 0:
+                s += '  '
+            s += k + ' ' + str(v)
+        return s
+
+    def tb_log(self, tb_logger, prefix='', step=None):
+        """Log using tensorboard
+        """
+        for k, v in self.meters.iteritems():
+            tb_logger.log_value(prefix + k, v.val, step=step)
+
